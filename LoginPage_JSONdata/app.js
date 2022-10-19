f38f666254d340cf2f4a7137790d693f9ec9e597
Rohitx03 <rohithasnani03@gmail.com>
2022-10-19 14:33:54 +0530

    first commit

---
+++ LoginPage_JSONdata/app.js
@@ -0,0 +1,23 @@
+let form=document.getElementById("loginform");
+form.addEventListener('click',btnEventHandler);
+function btnEventHandler(){
+    fetch('./data.json',)
+       .then(res=>  res.json())
+       .then(data=>{
+        console.log(data)
+       } )
+//     const formE1=document.querySelector('.form');
+//     formE1.addEventListener('submit', event=>{
+//     const formData= new formData(form);
+//     const data= Object.fromEntries(formData);
+
+// fetch('http://localhost:3000/data',{
+//     method:'POST',
+//     headers:{
+//         'Content-Type':'Application/json'
+//     }, body:JSON.stringify(data)
+// }).then(res=> res.json())
+//     console.log(res)
+//   .then(data=>console.log(data))
+//     });
+    }
